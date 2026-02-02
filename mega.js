af6b9e0d1d25bc73679b8915e8a9eca5d14925e6
jaber346 <jaberporgo@gmail.com>
2026-02-02 13:43:23 +0000

    mega.js

--- mega.js
+++ mega.js
@@ -4,7 +4,7 @@ import { Storage } from 'megajs';
  * ⚠️ REMPLACE ICI
  */
 const email = 'jaberporgo@gmail.com';
-const password = 'Djabir133';
+const password = 'Djabir123';
 
 const userAgent =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
